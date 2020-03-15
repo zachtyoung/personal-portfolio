cddeb846d280c53de7290904dfd7b68d64df8429
zachtyoung <ztyoung59@yahoo.com>
2020-03-15 02:46:15 -0500

    Sanity Commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {Landing} from './components/Landing'
+
+function App() {
+  return (
+    <div className="App">
+         <Router>
+        <Navbar/>
+         <Route exact path='/'component={Landing}/>
+       </Router>
+    </div>
+  );
+}
+
+export default App;
