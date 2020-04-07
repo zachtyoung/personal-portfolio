cafa5ed8c4056e8bab5206f5ab50f1a444b8a1eb
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-04-07 13:32:49 -0500

    WIP

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,9 +1,22 @@
 import React, {useEffect, useState} from 'react';
+import profile from '../assets/profile.png'
+import scroll from '../assets/Scroll.png'
 
 export const About = () => {
 
     return(
-        <h1>About goes here</h1>
+        <div className="about">
+            <div className='about-wrap'>
+            <div className='about-l'>
+            <h1>Zach Young</h1>
+            <p>Hello, my name is Zach Young. <br></br>I am a <span className="blue">Full Stack Software Engineer</span>.</p>
+            </div>
+            <div className='about-R'>
+                <img src={profile}></img>
+            </div>
+            </div>
+            <img src={scroll} className='scroll'></img>
+        </div>
     )
 
 }
--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,9 +1,35 @@
 import React, {useEffect, useState} from 'react';
+import emailjs from 'emailjs-com';
+import send from '../assets/send.svg'
 
 export const Contact = () => {
 
-    return(
-        <h1>Contact goes here</h1>
-    )
+
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('default_service', 'template_Dm0M7G6m', e.target, 'user_oQHUVz3lGdFUG7yN7jcWa')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+    
+      return (
+
+        <div id="contact">
+            <h2>Get in Touch</h2>
+            <p>Leave your message and I'll get back to you as soon as possible.</p>
+            <form className="contact-form" onSubmit={sendEmail}>
+              <input type="hidden" name="contact_number" />
+              <input type="text" name="user_name" placeholder='Name'/>
+              <input type="email" name="user_email" placeholder='Email'/>
+              <textarea name="message" placeholder='Message'/>
+              <button type="submit">Send<img className='send'src={send}></img></button>
+            </form>
+        </div>
+      );
 
 }
