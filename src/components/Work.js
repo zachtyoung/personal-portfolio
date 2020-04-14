4316ebacc601bcea0152f34c611c01bc517d4483
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-04-14 13:07:19 -0500

    WIP

--- src/components/Work.js
+++ src/components/Work.js
@@ -18,7 +18,14 @@ export const Work = () => {
                     <li><a title='View Project' href='https://ecbuild.netlify.com/'><p>Demo</p><img className='work-link' src={live}></img></a></li>
                     <li><a title='View Code' href='https://github.com/Build-Week-Empowered-Conversations'><p>Code</p><img className='work-link' src={github}></img></a></li>
                 </ul>
-                <img className='work-img' src={empowered}></img>
+                <div className='work-img'>
+                    <img className='work-img-img' src={empowered}></img>
+                    <div class="overlay">
+                        <div class="text">Anonymously message a loved one with education materials about a difficult conversation you would like to have with them.<br></br><br></br>When that loved one finished their curriculum, a message will be sent back to the original sender telling them they are prepared for their conversation.</div>
+                    </div>
+                </div>
+                
+
             </div>
             <div className="work2 clearfix">
                 <div className='workText'>
@@ -29,7 +36,13 @@ export const Work = () => {
                     <li><a title='View Project' href='https://diabetesmanager.netlify.com'><img className='work-link' src={live}></img><p>Demo</p></a></li>
                     <li><a title='View Code' href='https://github.com/Build-Week-Empowered-Conversations'><img className='work-link' src={github}></img><p>Code</p></a></li>
                 </ul>
-                <img className='work-img' src={insuline}></img>
+                <div className='work-img'>
+                <img className='work-img-img' src={insuline}></img>
+                    <div class="overlay">
+                        <div class="text">Anonymously message a loved one with education materials about a difficult conversation you would like to have with them.<br></br><br></br>When that loved one finished their curriculum, a message will be sent back to the original sender telling them they are prepared for their conversation.</div>
+                    </div>
+                </div>
+                
             </div>
             <div className="work clearfix">
                 <div className='workText'>
@@ -40,7 +53,12 @@ export const Work = () => {
                     <li><a title='View Project' href='https://www.d8picker.com/'><p>Demo</p><img className='work-link' src={live}></img></a></li>
                     <li><a title='View Code' href='https://github.com/Lambda-School-Labs/school-calendar-fe'><p>Code</p><img className='work-link' src={github}></img></a></li>
                 </ul>
-                <img className='work-img' src={school}></img>
+                <div className='work-img'>
+                <img className='work-img-img' src={school}></img>
+                    <div class="overlay">
+                        <div class="text">Anonymously message a loved one with education materials about a difficult conversation you would like to have with them.<br></br><br></br>When that loved one finished their curriculum, a message will be sent back to the original sender telling them they are prepared for their conversation.</div>
+                    </div>
+                </div>
             </div>
             
             <div className="work2 clearfix">
@@ -52,7 +70,14 @@ export const Work = () => {
                     <li><a title='View Project' href='https://pricetracker.netlify.com/'><img className='work-link' src={live}></img><p>Demo</p></a></li>
                     <li><a title='View Code' href='https://github.com/zachtyoung/price-tracker-fe'><img className='work-link' src={github}></img><p>Code</p></a></li>
                 </ul>
-                <img className='work-img' src={price}></img>
+                
+                
+                <div className='work-img'>
+                <img className='work-img-img' src={price}></img>
+                    <div class="overlay">
+                        <div class="text">Anonymously message a loved one with education materials about a difficult conversation you would like to have with them.<br></br><br></br>When that loved one finished their curriculum, a message will be sent back to the original sender telling them they are prepared for their conversation.</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
